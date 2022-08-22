3cf5f35ec173c7bc86792c7760bf505efc6c592e
Guillaume Cazorla <cazorla.guillaume@gmail.com>
2022-08-22 16:44:47 +0200

    creation des relation entre:  Date ->  Etat  ManytoOne  Lieux -> Ville ManytoOne  Date -> Lieux ManytoOne

--- ENI-SYMFONY-GR5/src/Entity/Etat.php
+++ ENI-SYMFONY-GR5/src/Entity/Etat.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\EtatRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,17 @@ class Etat
      */
     private $libelle;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Date::class, mappedBy="etatDate", orphanRemoval=true)
+     */
+
+    private $dates;
+
+    public function __construct()
+    {
+        $this->dates = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +48,36 @@ class Etat
     public function setLibelle(string $libelle): self
     {
         $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Date>
+     */
+    public function getDates(): Collection
+    {
+        return $this->dates;
+    }
+
+    public function addDate(Date $date): self
+    {
+        if (!$this->dates->contains($date)) {
+            $this->dates[] = $date;
+            $date->setEtatDate($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDate(Date $date): self
+    {
+        if ($this->dates->removeElement($date)) {
+            // set the owning side to null (unless already changed)
+            if ($date->getEtatDate() === $this) {
+                $date->setEtatDate(null);
+            }
+        }
 
         return $this;
     }
